1daf6191c16d42e966188b2817428483e7b2f597
Kevin <kecoma2@gmail.com>
2023-10-08 11:32:28 +0200

    Daily bot

--- DailyBot.py
+++ DailyBot.py
@@ -8,6 +8,8 @@ api_key = 'mlnsfeYmkrf4rh8tJnZtttCQ5DYNcLZ61Ujc0RBqSWxh0477J3YSlJ8zKpshSzTl'
 api_secret = 'wLqsXNCgU9mIUe0OtScueIQBcM9acV2PuEvF3deYqhGPnUJxyETcCQKMAkjyTBNe'
 base_url = 'https://testnet.binance.vision/api/v3/order'
 
+quantity = 20
+
 while True:
     # Parámetros de la solicitud
     params = {
@@ -15,7 +17,7 @@ while True:
         'side': 'BUY',
         'type': 'MARKET',
         'timestamp': int(time.time() * 1000),
-        'quantity': 50,
+        'quantity': quantity,
     }
 
     # Crear cadena de consulta ordenada
@@ -31,6 +33,6 @@ while True:
     response = requests.post(base_url, params=params, headers={'X-MBX-APIKEY': api_key})
 
     now = datetime.now()
-    print(f'Comprados 50 ADA {now.strftime("%m/%d/%Y, %H:%M:%S")}')
+    print(f'Comprados {quantity} ADA {now.strftime("%m/%d/%Y, %H:%M:%S")}')
 
     time.sleep(24*3600)
